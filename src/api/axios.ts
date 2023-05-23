d545c025f8b55b20b4417225aa6461f9b7587718
Van Vincent Vasquez <vanvasquez0725@gmail.com>
2023-05-23 13:03:51 +0800

    hssh

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 
-let BASE_URL
-
+let BASE_URL 
 if(process.env.NODE_ENV === 'production') {
   BASE_URL = 'https://jpadev-server.vercel.app/api/v1'
 }
 else {
   BASE_URL = 'http://localhost:1000/api/v1'
 }
+
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
